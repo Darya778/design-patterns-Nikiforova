e7dc9eeb8a97487a66316b7e0ac6e800f92aa804
Darya778 <112837727+Darya778@users.noreply.github.com>
2025-10-31 15:50:10 +0800

    Update warehouse_model.py

--- task2/src/models/warehouse_model.py
+++ task2/src/models/warehouse_model.py
@@ -6,5 +6,15 @@ from src.core.abstract_reference import abstract_reference
 
 """ Справочная модель склада """
 class warehouse_model(abstract_reference):
-    def __init__(self, name: str = ""):
+    def __init__(self, name: str = "", code: str = None):
         super().__init__(name)
+        self.code = code or self._generate_code(name)
+
+    def _generate_code(self, name: str):
+        return name[:3].upper().replace(" ", "")
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "code": self.code
+        }
