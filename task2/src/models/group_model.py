c7fe2c2bb220cbe91a27eaf8548b33e22a11aad7
Darya778 <112837727+Darya778@users.noreply.github.com>
2025-11-03 18:07:02 +0800

    Update group_model.py

--- task2/src/models/group_model.py
+++ task2/src/models/group_model.py
@@ -1,10 +1,16 @@
 """
 Модель группы номенклатуры (справочная), наследуется от abstract_reference
 """
+
 from src.core.abstract_reference import abstract_reference
 
 """ Группа номенклатуры. На данный момент содержит только наименование """
 class group_model(abstract_reference):
-
     def __init__(self, name: str = ""):
         super().__init__(name)
+
+    def to_dict(self):
+        return {
+            "id": getattr(self, "id", None),
+            "name": self.name
+        }
